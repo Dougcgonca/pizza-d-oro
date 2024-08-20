cb9c6e83529016eb96fca29f7f443c7392fac8e2
Douglas Cabreira <dougc.gonca@gmail.com>
2024-08-20 20:39:14 -0300

    feat: init pizzeria project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { InitialScreenComponent } from './components/initial-screen/initial-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: InitialScreenComponent }];
